27dd6e16401ac27c84fc3407c226b258ed402066
Nathan Castro <nathanslc18@gmail.com>
2020-08-16 22:05:51 -0300

    issue 8

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,70 +1,92 @@
-import React, { useState } from 'react'
-import { TimeSplit } from './typings/global'
-import { tick } from './utils/time'
-import { useCssHandles } from 'vtex.css-handles'
-import { FormattedMessage } from 'react-intl'
-
-
-
+import { useCssHandles } from "vtex.css-handles";
+//import { FormattedMessage } from 'react-intl';
+import React, { useState } from 'react';
+import { TimeSplit } from './typings/global';
+import { tick } from './utils/time';
+import { useQuery } from 'react-apollo';
+// @ts-ignore
+import useProduct from 'vtex.product-context/useProduct'
+import productReleaseDate from './queries/productReleaseDate.graphql';
 interface CountdownProps {
-  title: string
+  //title: string
   targetDate: string
-}
-
-
-
-
-const DEFAULT_TARGET_DATE = (new Date('2020-08-18')).toISOString()
-
-const CSS_HANDLES = ["container", "countdown", "title"]
-//const CSS_HANDLES = ['countdown']
-
+};
+const DEFAULT_TARGET_DATE = (new Date('2020-09-25')).toISOString();
+//const CSS_HANDLES = ["container", "countdown", "title"]
+const CSS_HANDLES = ['countdown'];
 const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
-  title,
-  targetDate = DEFAULT_TARGET_DATE
-}) => {
-  const [timeRemaining, setTime] = useState<TimeSplit>({
+  }) => {
+  const [
+    timeRemaining,
+     setTime
+    ] = useState<TimeSplit>({
     hours: '00',
     minutes: '00',
-    seconds: '00',
-  })
-
-  const titleText = title || <FormattedMessage id="countdown.title" />
-  const handles = useCssHandles(CSS_HANDLES)
-
-  tick(targetDate, setTime)
-
-  return (
-    <div className={`${handles.container} t-heading-2 fw3 w-100 c-muted-1`}>
-      <div className={`${handles.title} t-heading-2 fw3 w-100 c-muted-1 db tc`}>{titleText}</div>
-      <div className={`${handles.countdown} t-heading-2 fw3 w-100 c-muted-1 db tc`}>
-        {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
+    seconds: '00'
+  });
+  const productContext = useProduct();
+  const product = productContext?.product;
+  const linkText = product?.linkText;
+  const { data, loading, error } = useQuery(productReleaseDate, {
+    variables: {
+      slug: linkText
+    },
+    ssr: false,
+    skip: !linkText,
+  });
+  const handles = useCssHandles(CSS_HANDLES);
+  tick(data?.product?.releaseDate || DEFAULT_TARGET_DATE, setTime);
+  if (loading) {
+    return (
+      <div>
+        <span>Loading...</span>
       </div>
+    );
+  };
+  if (error) {
+    return (
+      <div>
+        <span>Error!</span>
+      </div>
+    );
+  };
+  if (!product) {
+  return (
+    <div>
+      <span>Não há contexto de produto</span>
     </div>
   )
 }
-
-
+  //const titleText = title || <FormattedMessage id="countdown.title" />
+  //const handles = useCssHandles(CSS_HANDLES)
+  tick(data?.product?.releaseDate || DEFAULT_TARGET_DATE, setTime);
+  return (
+    //<div className={`${handles.countdown} t-heading-2 fw3 w-100 c-muted-1 db tc`}></div>
+    //<div className={`${handles.container} t-heading-2 fw3 w-100 c-muted-1`}>
+      //<div className={`${handles.title} db tc`}>
+      <div className={`${handles.countdown} t-heading-2 fw3 w-100 c-muted-1 db tc`}>
+        {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
+      </div>
+  );
+};
 Countdown.schema = {
-	title: 'editor.countdown.title',
-	description: 'editor.countdown.description',
-	type: 'object',
-	properties: {
-	    title: {
-	     title: 'Sou um título',
-	     type: 'string',
-	     default: null,
-	    },
-	    targetDate: {
-	      title: 'Data final',
-	      description: 'Data final usada no contador',
-	      type: 'string',
-	      default: null,
-	    },
-    },
-}
-
-
+  título: 'editor.countdown.title',
+  descrição: 'editor.countdown.description',
+  tipo: 'objeto',
+  propriedades: {
+    //title: {
+     // title: 'I am a title',
+    //  type: 'string',
+     // default: null,
+    //},
+    targetDate: {
+      title: 'Data final',
+      descrição: 'Data final usada na contagem regressiva',
+      type: 'string',
+      default: null,
+    }
+  }
+};
 
 
 export default Countdown
